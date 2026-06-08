9b84c06eec9bb955c6ae27fefc064a23abaa435d
asklfxue <hkyle4658@gmail.com>
2026-06-08 21:14:55 +0100

    Map legend finished, map glitches fixed, map styling changed, moving onto skeleton loaders

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -8,12 +8,14 @@ import Map from "./components/Map"
 import { useState } from "react"
 import LocationDropdown from "./components/dropdowns/LocationDropdown"
 import MapTypeDropdown from "./components/dropdowns/MapTypeDropdown"
+import MapLegend from "./components/MapLegend"
 
 
 function App() {
 
   const [location, setLocation] = useState({ lat: 51.5074, lon: -0.1278 })
   const [mapType, setMapType] = useState('clouds_new')
+  const [selectedCity, setSelectedCity] = useState('London')
   
   const {data} = useQuery({
     queryKey: ['weather', location.lat, location.lon],
@@ -25,14 +27,22 @@ function App() {
       <div className="flex gap-8">
         <div className="flex gap-4">
           <h1 className="text-2xl font-semibold">Location: </h1>
-          <LocationDropdown setLocation={setLocation} />
+          <LocationDropdown 
+          setLocation={setLocation} 
+          selectedCity={selectedCity}
+          setSelectedCity={setSelectedCity} />
         </div>
         <div className="flex gap-4">
           <h1 className="text-2xl font-semibold">Map Type:</h1>
-          <MapTypeDropdown mapType={mapType} setMapType={setMapType} />
+          <MapTypeDropdown 
+          mapType={mapType} 
+          setMapType={setMapType}/>
         </div>
       </div>
-      <Map setLocation={setLocation} location={location} mapType={mapType} />
+      <div className="relative">
+        <Map setLocation={setLocation} location={location} mapType={mapType} setSelectedCity={setSelectedCity} />
+        <MapLegend mapType={mapType}/>
+      </div>
       <CurrentWeather lat={location.lat} lon={location.lon} />
       <HourlyForecast lat={location.lat} lon={location.lon} />
       <DailyForecast lat={location.lat} lon={location.lon} />
